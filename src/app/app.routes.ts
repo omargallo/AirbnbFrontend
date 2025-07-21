9592b7f2acc5e2c6c93361cf7988ce5ccaae2954
Omar Khaled <omarkhhamad22@gmail.com>
2025-07-21 04:28:42 +0300

    initial create listing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,13 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 
+import { ListingWizardComponent } from './pages/listing-wizard/listing-wizard';
+import { MainLayout } from './layout/main-layout/main-layout';
+
 export const routes: Routes = [
-    {path:"", component:Home}
+    { path: '', component: MainLayout,
+        children: [{path: '', component: Home}]
+     },
+    { path: 'listing-wizard', component: ListingWizardComponent }
+
 ];
