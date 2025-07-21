1e156c966c166eec403c88b1afff131097fbb316
mahmoud140106 <amer140106@gmail.com>
2025-07-21 07:56:39 +0300

    updates

---
+++ src/app/pages/filterpage/filterpage.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filterpage',
+  imports: [],
+  templateUrl: './filterpage.html',
+  styleUrl: './filterpage.css'
+})
+export class Filterpage {
+
+}
