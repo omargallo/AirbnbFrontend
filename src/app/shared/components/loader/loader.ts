74c20a75427290472676d7cffe295bea14eb4e88
Ahmed Essam <ahmed.essam.aladl@gmail.com>
2025-07-22 21:49:19 +0300

    Update Withlist modal

--- src/app/shared/components/loader/loader.ts
+++ src/app/shared/components/loader/loader.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-loader',
-  imports: [],
-  templateUrl: './loader.html',
-  styleUrl: './loader.css'
+  imports:[CommonModule],
+  templateUrl:"./loader.html" ,
+  styleUrls: ['./loader.css']
 })
+
+
 export class Loader {
 
+  @Input() size: 'sm' | 'md' | 'lg' | 'xl' | 'custom' = 'md';
+  @Input() color: 'primary' | 'secondary' | 'success' | 'danger' | 'warning' | 'info' | 'light' | 'dark' = 'primary';
+  @Input() loadingText: string = 'Loading...';
+  @Input() showText: boolean = false;
+  @Input() textColor: 'primary' | 'secondary' | 'success' | 'danger' | 'warning' | 'info' | 'light' | 'dark' | 'muted' = 'muted';
+  @Input() customSize: string = '';
+  @Input() borderWidth: string = '0.25em';
+  @Input() containerHeight: string = 'auto';
+  @Input() containerClass:any
+
+  get sizeClass(): string {
+    switch (this.size) {
+      case 'sm': return 'spinner-border-sm';
+      case 'lg': return '';
+      case 'xl': return '';
+      case 'custom': return '';
+      default: return '';
+    }
+  }
 }
+
