a03da418e813512f71a51a59e6bdf99bb01c15dd
dawood74 <omardaved75@gmail.com>
2025-07-22 03:16:29 +0300

    add cmp

---
+++ src/app/pages/update-list/update-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-list',
+  imports: [],
+  templateUrl: './update-list.html',
+  styleUrl: './update-list.css'
+})
+export class UpdateList {
+
+}
