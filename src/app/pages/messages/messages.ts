f6b874ed3d81dfc0f532b1452812fa3beafa1a04
Ahmed Essam <ahmed.essam.aladl@gmail.com>
2025-08-02 09:18:55 +0300

    Chat ReservationBox

--- src/app/pages/messages/messages.ts
+++ src/app/pages/messages/messages.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
 import { MessagesBoxComponent } from "../../components/messages-box/messages-box";
 import { ChatBoxComponent } from "../../components/chat-box/chat-box";
 import { ReservationBoxComponent } from "../../components/reservation-box/reservation-box";
@@ -21,7 +21,7 @@ export class Messages implements OnInit, OnDestroy {
   initialMessages: MessageDto[] = [];
   ReservationWithProperty: any | null = null;
   isLoadingChat: boolean = false;
-
+  currentOpenChatId?:string
   isHost: boolean = false;
 
 
@@ -30,7 +30,9 @@ export class Messages implements OnInit, OnDestroy {
 
   constructor(
     private signalRService: SignalRService,
-    private messageService: ChatService, private authService: AuthService) { }
+    private messageService: ChatService, private authService: AuthService,
+    private cdr: ChangeDetectorRef
+) { }
 
   ngOnInit(): void {
     this.startSignalRConnection();
@@ -51,6 +53,9 @@ export class Messages implements OnInit, OnDestroy {
   }
 
   onChatSessionSelected(session: ChatSessionDto): void {
+    if(this.currentOpenChatId == session.id)
+      return
+    this.currentOpenChatId = session.id
     if (session.hostId === this.currentUserId) {
       this.isHost = true;
     } else {
@@ -74,18 +79,23 @@ export class Messages implements OnInit, OnDestroy {
     };
 
     console.log(reserveRequest)
+    const start = performance.now()
     if (!this.isHost) {
       this.messageService.getSessionForHost(session.id).subscribe({
         next: (res) => {
           console.log("reserveRequest", reserveRequest)
           console.log("res", res.data)
           // Add slight delay for better UX
+          const end = performance.now();
+          console.log(`Execution time: ${end - start} ms`);
+          
           setTimeout(() => {
             this.selectedChatSession = res?.data?.chatSession;
             this.initialMessages = res?.data?.messages;
             this.ReservationWithProperty = res?.data;
             this.isLoadingChat = false;
-          }, 200);
+            this.cdr.detectChanges()
+          }, 0);
           console.log("from next !ishost",this.authService.userId)
           
         },
@@ -98,28 +108,32 @@ export class Messages implements OnInit, OnDestroy {
     } else {
       this.selectedChatSession = session;
       // this.messageService.getChatMessages(session.id, 1, 50).subscribe({
-      //   next: (messages: MessageDto[]) => {
-      //     this.initialMessages = messages || [];
-      //     this.isLoadingChat = false;
-      //   },
-      //   error: (err) => {
-      //     console.error('Error loading host messages:', err);
-      //     this.isLoadingChat = false;
-      //     this.initialMessages = [];
-      //   }
-      // });
-
-      this.messageService
+        //   next: (messages: MessageDto[]) => {
+          //     this.initialMessages = messages || [];
+          //     this.isLoadingChat = false;
+          //   },
+          //   error: (err) => {
+            //     console.error('Error loading host messages:', err);
+            //     this.isLoadingChat = false;
+            //     this.initialMessages = [];
+            //   }
+            // });
+            
+            this.messageService
             .getSessionForHost(session.id)
             .subscribe({
               next:(res)=>{
                 console.log("chat for host ",res.data)
+                const end = performance.now();
+                console.log(`Execution time: ${end - start} ms`);
                 setTimeout(() => {
-                    this.selectedChatSession = res?.data?.chatSession;
-                    this.initialMessages = res?.data?.messages;
-                    this.ReservationWithProperty = res?.data;
-                    this.isLoadingChat = false;
-                  }, 200);
+                  this.selectedChatSession = res?.data?.chatSession;
+                  this.initialMessages = res?.data?.messages;
+                  this.ReservationWithProperty = res?.data;
+                  this.isLoadingChat = false;
+                  this.cdr.detectChanges()
+
+                }, 0);
               },
               error:(res)=>{
                   console.log(res)
